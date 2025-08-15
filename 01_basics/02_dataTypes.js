c980998b1280475a5da962797ba1f6b19f4737e6
GuptaRohit2001 <imrohitgupta2001@gmail.com>
2025-08-15 19:37:13 +0530

    data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) This can be used in browser not in node
+
+console.log(3+3)
+
+let name="Rohit"
+let age=24
+let isLoggedIn=false
+
+//number=> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+// symbol => unique
+
+console.log(typeof(age))
+console.log(typeof(null)) //object
+console.log(typeof(undefined))
