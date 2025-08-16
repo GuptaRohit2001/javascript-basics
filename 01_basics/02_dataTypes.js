cbe0e97506b6155bb73b71f2cf52d9fc6640eaff
GuptaRohit2001 <imrohitgupta2001@gmail.com>
2025-08-16 10:56:42 +0530

    conversion of datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,11 +1,11 @@
 "use strict"; //treat all js code as newer version
 //alert(3+3) This can be used in browser not in node
 
-console.log(3+3)
+// console.log(3+3)
 
-let name="Rohit"
-let age=24
-let isLoggedIn=false
+// let name="Rohit"
+// let age=24
+// let isLoggedIn=false
 
 //number=> 2 to power 53
 //bigint
@@ -15,6 +15,38 @@ let isLoggedIn=false
 //undefined
 // symbol => unique
 
-console.log(typeof(age))
-console.log(typeof(null)) //object
-console.log(typeof(undefined))
+// console.log(typeof(age))
+// console.log(typeof(null)) //object
+// console.log(typeof(undefined))
+
+
+// let score="33abc"
+
+// let score1=null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+// console.log(score1)
+// let valueInNumber1=Number(score1)
+// console.log(typeof(valueInNumber1))
+// console.log(valueInNumber1)
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1,false=>0
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1=> true
+//0=> false
+// "hitesh"=> true
+// ""=>false
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
