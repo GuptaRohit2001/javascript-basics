9214ea6bc2938cc427bc87b9196fb87de76fdfd3
GuptaRohit2001 <imrohitgupta2001@gmail.com>
2025-08-17 09:03:38 +0530

    math and number

---
+++ 01_basics/03_string.js
@@ -0,0 +1,46 @@
+// name="Rohit"
+// repoCount=5
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName=new String('Rohit-Gupta-com')
+
+// console.log(gameName[0])
+
+// console.log(gameName.length)
+// console.log(gameName.toLocaleUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+// const newString=gameName.substring(0,3)
+// console.log(newString);
+
+// const anotherString=gameName.slice(-8,4)
+// console.log(anotherString);
+
+// const newStringOne="    rohit    "
+// console.log(newStringOne.trim())
+// console.log(newStringOne)
+// console.log(newStringOne.trimStart())
+// console.log(newStringOne.trimEnd())
+
+// const url="https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('sundar'))
+// console.log(gameName.split('-'))
+
+// const score=400;
+// console.log(score);
+
+// const balance=new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+// const otherName=123.8966
+// console.log(otherName.toPrecision(4))
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString());
+
+//++++++++++++++ Math ++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
---
+++ 04_math_number.js
@@ -0,0 +1,31 @@
+const score=400
+const balance=new Number(100)
+console.log(balance)
+console.log(score)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherName=123.8966
+console.log(otherName.toPrecision(4))
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+//++++++++++++++++++++ Maths +++++++++++++++
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(5.9))
+console.log(Math.min(4,6,3,8))
+console.log(Math.max(45,10,-9,0))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
