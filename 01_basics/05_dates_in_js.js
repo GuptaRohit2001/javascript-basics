c47163d2594e45290760c3cee3dae1a61d469e57
GuptaRohit2001 <imrohitgupta2001@gmail.com>
2025-08-18 21:21:59 +0530

    arrays completed

---
+++ 01_basics/05_dates_in_js.js
@@ -0,0 +1,21 @@
+let myDate=new Date()
+
+console.log(myDate.toTimeString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toDateString())
+console.log(myDate.toString())
+console.log(typeof myDate)
+
+let num=new Number(500)
+console.log(num)
+
+// let myCreatedDate=new Date(2023,0,26)
+// let myCreatedDate=new Date(2023,0,23,5,3)
+let myCreatedDate=new Date("2023-01-14")
+// let myCreatedDate=new Date("01-14-2023")
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
